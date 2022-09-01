ae5741e1a23f83db5314fd6342643d12d08b27ea
MatheusVict <matheusvictorhenrique@gmail.com>
2022-09-01 17:13:17 -0300

    useRef2

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,14 @@
+import { useRef } from "react"
+
 export const Dashboard = () => {
+    const contaClick = useRef({ counter: 0 }); // não precisa passar um parametro de tipagem pq ta iniciando co o valor padrão o objeto
+    // armaeza o novo valor sem fazer um re render
     return(
         <>
            <h1>Dashboard</h1> 
+           <p>Contador : {contaClick.current.counter}</p>
+           <button onClick={() => contaClick.current.counter++}>Adicionar</button>
+           <button onClick={() => console.log(contaClick.current.counter)}>Mostrar</button>
         </>
     )
 }
