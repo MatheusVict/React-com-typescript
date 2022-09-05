10187d4cf7eb60f94d236d17becd1c71c0fc6d70
MatheusVict <matheusvictorhenrique@gmail.com>
2022-09-05 15:09:44 -0300

    Criando um ReactHook

---
+++ src/app/hooks/UserLog.ts
@@ -0,0 +1,10 @@
+import { useCallback, useContext } from "react"
+import { UserLogContext } from "../context/Userlog";
+ // React hooks = função q usa hooks do react
+
+export const useUserLog = () => { // Meu hook personalzado
+    const context = useContext(UserLogContext);
+    return context
+};
+
+// Criei meu proprio hook pra não precisar ficar importando tudo
--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,10 @@
-import { useContext, useRef } from "react"
-import { UserLogContext } from "../../context/Userlog";
+import { useRef } from "react"
+import { useUserLog } from "../../hooks/UserLog";
 
 export const Dashboard = () => {
     const contaClick = useRef({ counter: 0 }); // não precisa passar um parametro de tipagem pq ta iniciando co o valor padrão o objeto
     // armaeza o novo valor sem fazer um re render
-    const LoginUserContext = useContext(UserLogContext);
+    const LoginUserContext = useUserLog()
     return(
         <>
            <h1>Dashboard</h1>
